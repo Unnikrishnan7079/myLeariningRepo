d41edd62e2895a0f8ac9db39d5c5881bdae9ff97
Unnikrishnan7079 <unnikrishnanm7079@outlook.com>
2021-08-12 14:03:40 +0530

    My second commt

--- nodeTestv0.js
+++ nodeTestv0.js
@@ -6,3 +6,10 @@ let myArray =
 
 myArray = myArray.filter((item)=>item.age > 23)
 console.log("Myarray is ", myArray)
+
+let newArray = myArray.reduce((acc,curr)=> ({age: acc.age + curr.age}))
+console.log("MyaArray is ",newArray)
+
+var arr = [{x:1},{x:2},{x:4}];
+let newArr = arr.reduce((a, b) => ({x: a.x + b.x}));
+console.log("New is ",newArr)
